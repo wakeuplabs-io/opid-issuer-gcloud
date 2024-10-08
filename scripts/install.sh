9feaaaa95efe8f7d1e1c5bb345d5612a5b87cf67
matzapata <matuzapata@gmail.com>
2024-10-08 22:37:17 +0700

    cleanup

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,20 @@
-source ./secrets.sh
+source ./scripts/secrets.sh
 helm install "$APP_INSTANCE_NAME" chart/optimism-id-issuer \
-  --create-namespace  --namespace "$NAMESPACE" \
-    --set appdomain="$APP_DOMAIN" \
-    --set uidomain="$UI_DOMAIN" \
-    --set apidomain="$API_DOMAIN" \
-  --set privatekey="$PRIVATE_KEY" \
-  --set issuerName="$ISSUER_NAME" \
-  --set uiPassword="$UI_PASSWORD" \
+  --create-namespace --set namespace="$NAMESPACE" \
   --set mainnet="$MAINNET" \
-  --set ethereumUrl="$ETHEREUM_URL" \
-  --set ingressEnabled="$INGRESS_ENABLED" \
+  --set issuerEthereumUrl="$RPC_URL" \
+  --set uiUsername="$UI_USERNAME" \
+  --set uiPassword="$UI_PASSWORD" \
+  --set apiUiUsername="$API_UI_USERNAME" \
+  --set apiUiPassword="$API_UI_PASSWORD" \
+  --set apiUser="$API_USERNAME" \
+  --set apiPassword="$API_PASSWORD" \
+  --set staticIpName="$STATIC_IP_NAME" \
+  --set apphost="$APP_HOST" \
+  --set uihost="$UI_HOST" \
+  --set apihost="$API_HOST" \
+  --set issuerName="$ISSUER_NAME" \
+  --set privateKey="$PRIVATE_KEY" \
   --set vaultpwd="$VAULT_PASSWORD" \
   --set rhsmode="$RHS_MODE" \
-  --set rhsurl="$RHS_URL"
+  --set rhsurl="$RHS_URL" 
--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,15 +1,14 @@
 source ./secrets.sh
 helm upgrade "$APP_INSTANCE_NAME" chart/optimism-id-issuer \
   --create-namespace  --namespace "$NAMESPACE" \
-    --set appdomain="$APP_DOMAIN" \
-    --set uidomain="$UI_DOMAIN" \
-    --set apidomain="$API_DOMAIN" \
+  --set appdomain="$APP_DOMAIN" \
+  --set uidomain="$UI_DOMAIN" \
+  --set apidomain="$API_DOMAIN" \
   --set privatekey="$PRIVATE_KEY" \
   --set issuerName="$ISSUER_NAME" \
   --set uiPassword="$UI_PASSWORD" \
   --set mainnet="$MAINNET" \
   --set ethereumUrl="$ETHEREUM_URL" \
-  --set ingressEnabled="$INGRESS_ENABLED" \
   --set vaultpwd="$VAULT_PASSWORD" \
   --set rhsmode="$RHS_MODE" \
   --set rhsurl="$RHS_URL"
